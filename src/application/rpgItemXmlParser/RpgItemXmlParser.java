392e4714668bb575c0f8569b6a0264bd1c2c12e4
Arthur Kautz <Ak182182@gmail.com>
2022-11-15 09:46:59 -0500

    Remove unused import

--- src/application/rpgItemXmlParser/RpgItemXmlParser.java
+++ src/application/rpgItemXmlParser/RpgItemXmlParser.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
